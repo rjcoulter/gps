d379e4a2a6a2a38fbaa6e5c3a3aa1e7293781e4b
rjcoulter <ryancoulter22@gmail.com>
2024-12-28 19:55:36 -0500

    Ascii art

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,18 +9,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gps",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A port scanner written in go",
+	Long: `
+   __ _ _ __  ___ 
+  / _  | '_ \/ __|
+ | (_| | |_) \__ \
+  \__, | .__/|___/
+  |___/|_|        
+		
+ gps is a port scanner for Go to help you pen test`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +46,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
